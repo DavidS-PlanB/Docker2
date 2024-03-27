80c2e3744858622a2db730dc6aff73778274a28b
David Schäfer <David.Schaefer@plan-b-gmbh.com>
2024-03-27 09:16:55 +0100

    n

--- Aufgabe2/Frontend/script.js
+++
@@ -1,4 +0,0 @@
-// Beispiel für die Kommunikation mit dem Backend
-fetch('http://backend-service:3000/data')
-  .then(response => response.json())
-  .then(data => console.log(data));
---
+++ Aufgabe2/Website/Backend/script.js
@@ -0,0 +1,21 @@
+
+function changeText() {
+  var element = document.getElementById('demo');
+  element.innerHTML = 'Text has been changed!';
+}
+
+function showMessage() {
+  alert('Hello! This is a message from JavaScript.');
+}
+
+function generateRandomNumber() {
+  var randomNumber = Math.floor(Math.random() * 100) + 1;
+  alert('Random Number: ' + randomNumber);
+}
+
+// Füge eine Startverzögerung von 5 Sekunden hinzu
+
+function startServer() {
+  // Füge hier den Code zum Starten deines Node.js-Servers ein
+  console.log('Starting Node.js server...');
+}
